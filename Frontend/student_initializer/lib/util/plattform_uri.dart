06a65e7d9512d9d5beaff271e59a17d54e4390af
Lukas Linde <linde.lukas.99@gmail.com>
2025-05-22 11:19:45 +0200

    student_initializer: chrome support

--- Frontend/student_initializer/lib/util/plattform_uri.dart
+++ Frontend/student_initializer/lib/util/plattform_uri.dart
@@ -4,6 +4,10 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class PlattformUri {
   static String getUri() {
+    const bool kIsWeb = bool.fromEnvironment('dart.library.js_util');
+    if (kIsWeb) {
+      return 'http://127.0.0.1:8081';
+    }
     if (Platform.isAndroid) {
       return dotenv.env['ANDROID_BASE_PERSISTANT_HTTP']!;
     }
